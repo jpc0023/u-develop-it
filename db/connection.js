27e34580c4f51417f30333beb959cba3509b9102
jpc0023 <91299930+jpc0023@users.noreply.github.com>
2022-03-07 00:02:36 -0600

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'Jcro$21083',
+    password: '',
     database: 'election'
   });
 
